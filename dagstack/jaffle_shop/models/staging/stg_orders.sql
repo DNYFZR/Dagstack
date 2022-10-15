1ac05b672409b5d06f000a676f649e181b1ba717
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-10-15 15:12:28 +0100

    Bugfix - dbt SQL syntax

--- dagstack/jaffle_shop/models/staging/stg_orders.sql
+++ dagstack/jaffle_shop/models/staging/stg_orders.sql
@@ -2,7 +2,7 @@
 -- https://docs.dagster.io/integrations/dbt/using-dbt-with-dagster/part-one
 
 SELECT 
-    id as order_id
+    id as order_id,
     user_id as customer_id,
     order_date,
     status
